faf4624ec3d8e1f6dec88b8fabdd60c863e8acd4
Tommy Tran <s4018825@student.rmit.edu.au>
2026-02-10 20:01:29 +1100

    string concatenation with primitives

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,8 @@ public class Main {
         int result = 10 + 12;
         String answer = "The answer is " + result;
         System.out.println(answer);
+
+        String howMany = 20 + " things";    
+        System.out.println(howMany);
     }
 }
