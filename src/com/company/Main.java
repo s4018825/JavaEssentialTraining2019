bc52b209772dac81fd0c2637bd12810ca310f895
Tommy Tran <s4018825@student.rmit.edu.au>
2026-02-11 13:33:56 +1100

    working with NumberFormat

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.text.NumberFormat;
+
 public class Main {
 
     public static void main(String[] args) {
         var doubleValue = 10_000_000.53;
+
+        var numF = NumberFormat.getNumberInstance();
+        System.out.println("Number: " + numF.format(doubleValue));
+
+        var intF = NumberFormat.getIntegerInstance();
+        System.out.println("Number: " + intF.format(doubleValue));
     }
 }
