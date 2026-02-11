8021cb9340013607824aa884ccc4dba2211e4ae5
Tommy Tran <s4018825@student.rmit.edu.au>
2026-02-11 13:52:15 +1100

    working with string formatting and interpolation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,24 @@ public class Main {
         var item = "Shirt";
         var size = "M";
         var price = 14.99;
-        var color = "Red";
+        var colour = "Red";
+
+        var template = "Clothing item: %s, size %s, colour %s, price $%.2f";  
+        var itemString = String.format(template, item, size, colour, price);
+        System.out.println(itemString);
+
+        var template2 = "%s climbs grade: %s at %s located at %s %d %s.";
+        String climberName = "Tommy Tran";
+        String climberGrade = "V4+";
+        String gymName = "Rocket Climbing Gym";
+        String gymSuburb = "Footscray";
+        var gymPostCode = 3011;
+        String stateCode = "VIC";
+
+        var climbingString = String.format(template2, climberName,
+            climberGrade, gymName, gymSuburb, gymPostCode, stateCode);
+
+        System.out.println(climbingString);
 
     }
 }
